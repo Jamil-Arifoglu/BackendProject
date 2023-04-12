c78eddcb26eacc6db20aa0ba48be3374802a4455
Jamil-Arifoglu <jamilai@code.edu.az>
2023-04-12 11:02:42 +0400

    BackendProject

---
+++ Foxic/Controllers/FoxicController.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Foxic.Controllers
+{
+    public class FoxicController : Controller
+    {
+        private readonly ILogger<FoxicController> _logger;
+
+        public FoxicController(ILogger<FoxicController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
